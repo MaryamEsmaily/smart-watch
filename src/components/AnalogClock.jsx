31155200c313a5cf3d4b9f43d4b82a20aeee3621
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-04-24 14:50:45 +0430

    fix main page

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Clock from "react-clock";
 import "react-clock/dist/Clock.css";
+
 function AnalogClock() {
   const [value, setValue] = useState(new Date());
+  const [date, setDate] = useState();
+
+  const today = new Date();
+  useEffect(() => {
+    setDate(
+      today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
+    );
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => setValue(new Date()), 1000);
@@ -12,8 +21,9 @@ function AnalogClock() {
     };
   }, []);
   return (
-    <div className="d-flex justify-content-center my-5">
-      <Clock value={value} className="d-block" />
+    <div className="d-flex flex-column align-items-center pt-5">
+      <Clock value={value} className="d-block mb-2" />
+      <div className="py-4 fs-1 fw-bolder">{date}</div>
     </div>
   );
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,22 +1,42 @@
 import React from "react";
+import { IoFastFoodOutline, IoFootstepsOutline } from "react-icons/io5";
+import { RiHeartPulseLine } from "react-icons/ri";
+import { TiWeatherPartlySunny } from "react-icons/ti";
 
 function Options() {
   return (
     <div className="container">
-      <div className="row text-center">
+      <div className="row">
         <div className="col-6">
-          <div className="py-5 my-2 bg-info rounded-3 text-nowrap">
-            HEART BEAT
+          <div
+            className="p-3 my-2 bg-info text-nowrap"
+            style={{ borderRadius: "15" }}
+          >
+            <RiHeartPulseLine className="fs-2" />
+            <p className="m-0 mt-4 fs-5">HEART RATE</p>
+            <p className="fw-lighter">Heart Rate</p>
           </div>
         </div>
         <div className="col-6">
-          <div className="py-5 my-2 bg-danger rounded-3"> STEPS</div>
+          <div className="p-3 my-2 bg-danger" style={{ borderRadius: "15" }}>
+            <IoFootstepsOutline className="fs-2" />
+            <p className="m-0 mt-4 fs-5">STEPS</p>
+            <p className="fw-lighter">Steps</p>
+          </div>
         </div>
         <div className="col-6">
-          <div className="py-5 my-2 bg-warning rounded-3"> WEATHER</div>
+          <div className="p-3 my-2 bg-warning" style={{ borderRadius: "15" }}>
+            <TiWeatherPartlySunny className="fs-2" />
+            <p className="m-0 mt-4 fs-5">WEATHER</p>
+            <p className="fw-lighter">Todays Weather</p>
+          </div>
         </div>
         <div className="col-6">
-          <div className="py-5 my-2 bg-primary rounded-3"> CALORIES</div>
+          <div className="p-3 my-2 bg-primary" style={{ borderRadius: "15" }}>
+            <IoFastFoodOutline className="fs-2" />
+            <p className="m-0 mt-4 fs-5">CALORIES</p>
+            <p className="fw-lighter">Calories</p>
+          </div>
         </div>
       </div>
     </div>
