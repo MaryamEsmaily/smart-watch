cb145182df43a9858772de1238ee4ae619a59c64
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-04-24 15:09:10 +0430

    add digital clock

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -5,25 +5,35 @@ import "react-clock/dist/Clock.css";
 function AnalogClock() {
   const [value, setValue] = useState(new Date());
   const [date, setDate] = useState();
+  const [time, setTime] = useState();
 
   const today = new Date();
   useEffect(() => {
     setDate(
       today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
     );
-  }, []);
+  }, [today]);
 
   useEffect(() => {
     const interval = setInterval(() => setValue(new Date()), 1000);
-
+    setTime(
+      today.getHours() +
+        ":" +
+        (today.getMinutes() < 10 ? "0" : "") +
+        today.getMinutes() +
+        ":" +
+        (today.getSeconds() < 10 ? "0" : "") +
+        today.getSeconds()
+    );
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [today]);
   return (
     <div className="d-flex flex-column align-items-center pt-5">
       <Clock value={value} className="d-block mb-2" />
-      <div className="py-4 fs-1 fw-bolder">{date}</div>
+      <div className="pt-4 fs-1 fw-bolder">{date}</div>
+      <div className="py-2 fs-4 ">{time}</div>
     </div>
   );
 }
